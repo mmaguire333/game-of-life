3456603e5a4d2b5db2e2a74b0a3a5ec27819eb1d
Matt <mmaguire333@gmail.com>
2023-02-05 15:01:02 -0700

    Create divs for the grid cells and add them to the grid container

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let container = document.querySelector('.grid-container');
+
+(function createGridCells(numRows, numCols) {
+    for(let i = 0; i < numRows; i++) {
+        for(let j = 0; j < numCols; j++) {
+            let newCell = document.createElement('div');
+            newCell.className = `cell row-${i} col-${j}`;
+            container.appendChild(newCell);
+        }
+    }
+})(30, 40);
